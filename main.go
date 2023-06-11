6c1633b94f1037db1b3e2e7b28268a7bf254bbb1
suryaa6666 <suryaelidanto@gmail.com>
2023-06-11 21:54:29 +0700

    first init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "Hello World!",
+		})
+	})
+
+	PORT := os.Getenv("PORT")
+
+	e.Logger.Fatal(e.Start(":" + PORT))
+}
